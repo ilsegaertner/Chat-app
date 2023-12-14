827e51a378afebb7cd1a5de6362bbf77979d3ffa
Ilse Gaertner <joachimpruegl@gmail.com>
2023-12-14 20:11:09 +0100

    Implement AsyncStorage / Netinfo

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import {
+  getFirestore,
+  enableNetwork,
+  disableNetwork,
+} from "firebase/firestore";
 
 import Start from "./components/Start.js";
 import Chat from "./components/Chat.js";
-import { LogBox } from "react-native";
+import { Alert, LogBox } from "react-native";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+import { useEffect } from "react";
+import { useNetInfo } from "@react-native-community/netinfo";
+
 //create the navigator
 const Stack = createNativeStackNavigator();
 
+// ignores error Message in Welcome Screen
+LogBox.ignoreLogs(["AsyncStorage has been extracted from"]);
+
 const App = () => {
-  // ignores error Message in Welcome Screen
-  LogBox.ignoreLogs(["AsyncStorage has been extracted from"]);
+  const connectionStatus = useNetInfo();
 
   // Your web app's Firebase configuration
   const firebaseConfig = {
@@ -31,12 +40,27 @@ const App = () => {
   // Initialize Cloud Firestore and get a reference to the service
   const db = getFirestore(app);
 
+  useEffect(() => {
+    if (connectionStatus.isConnected === false) {
+      Alert.alert("Connection lost!");
+      disableNetwork(db);
+    } else if (connectionStatus.isConnected === true) {
+      enableNetwork(db);
+    }
+  }, [connectionStatus.isConnected]);
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Start">
         <Stack.Screen name="Start" component={Start} />
         <Stack.Screen name="Chat">
-          {(props) => <Chat db={db} {...props} />}
+          {(props) => (
+            <Chat
+              db={db}
+              isConnected={connectionStatus.isConnected}
+              {...props}
+            />
+          )}
           {/* Using {...props} ensures that not only the db prop but also other relevant navigation-related props are passed to the Chat component. <Stack.Screen name="Chat" component={Chat} wouldnt pass the db props */}
         </Stack.Screen>
       </Stack.Navigator>
--- components/Chat.js
+++ components/Chat.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { StyleSheet, View, KeyboardAvoidingView, Platform } from "react-native";
-import { Bubble, GiftedChat } from "react-native-gifted-chat";
+import { Bubble, GiftedChat, InputToolbar } from "react-native-gifted-chat";
 import {
   collection,
   addDoc,
@@ -8,8 +8,9 @@ import {
   orderBy,
   query,
 } from "firebase/firestore";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const Chat = ({ navigation, route, db }) => {
+const Chat = ({ navigation, route, db, isConnected }) => {
   const { user, background, userID } = route.params;
   const [messages, setMessages] = useState([]);
 
@@ -35,31 +36,60 @@ const Chat = ({ navigation, route, db }) => {
     );
   };
 
-  useEffect(() => {
-    navigation.setOptions({ title: user });
-    const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
-    const unsubMessages = onSnapshot(q, (docs) => {
-      let newMessages = [];
-      docs.forEach((doc) => {
-        newMessages.push({
-          id: doc.id,
-          ...doc.data(),
-          createdAt: new Date(doc.data().createdAt.toMillis()),
-        });
-      });
-      setMessages(newMessages);
-    });
+  const renderInputToolbar = (props) => {
+    if (isConnected === true) return <InputToolbar {...props} />;
+    else return null;
+  };
 
-    return () => {
+  let unsubMessages; // If you leave let unsubShoppinglists in useEffect(), you’ll lose the reference to the old unsubscribe function. This is because re-initializing any kind of listener doesn’t override and remove the old listener, it only removes the reference to that old listener, while keeping it alive somewhere in the memory without a way of reaching it, resulting in a memory leak.
+
+  useEffect(() => {
+    if (isConnected === true) {
       if (unsubMessages) unsubMessages();
+      unsubMessages = null;
+
+      navigation.setOptions({ title: user });
+      const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
+      unsubMessages = onSnapshot(q, (docs) => {
+        let newMessages = [];
+        docs.forEach((doc) => {
+          newMessages.push({
+            id: doc.id,
+            ...doc.data(),
+            createdAt: new Date(doc.data().createdAt.toMillis()),
+          });
+        });
+        cacheMessages(newMessages);
+        setMessages(newMessages);
+      });
+    } else loadCachedMessages();
+
+    // Cleanup code
+    return () => {
+      if (unsubMessages) unsubMessages(); // This invocation is meant to stop listening to changes in the Firestore collection initiated by the onSnapshot call earlier in the useEffect. It's not a traditional unsubscribe, but rather a way to stop the listening process initiated by onSnapshot.
     };
-  }, []);
+  }, [isConnected]);
+
+  const loadCachedMessages = async () => {
+    // AsyncStorage methods are async and you will use await with them, so you can't use them straight away inside the useEffect() of the ShoppingLists component
+    const cachedMessages = (await AsyncStorage.getItem("messages")) || []; // "|| []" will assign an empty array to cachedLists in case AsyncStorage.getItem("shopping_lists") fails when the shopping_lists item hasn’t been set yet in AsyncStorage
+    setMessages(JSON.parse(cachedMessages));
+  };
+
+  const cacheMessages = async (messagesToCache) => {
+    try {
+      await AsyncStorage.setItem("messages", JSON.stringify(messagesToCache));
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   return (
     <View style={[styles.container, { backgroundColor: background }]}>
       <GiftedChat
         messages={messages}
         renderBubble={renderBubble}
+        renderInputToolbar={renderInputToolbar}
         onSend={(messages) => onSend(messages)}
         user={{
           _id: userID,
